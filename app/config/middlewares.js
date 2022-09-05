36a3256c5172431a1812440745b6bd3335de5067
Chris Chan <chris.chan@greenpeace.org>
2022-09-05 12:35:33 +0800

    Revert CORS setting

--- app/config/middlewares.js
+++ app/config/middlewares.js
@@ -1,14 +1,7 @@
 module.exports = [
   "strapi::errors",
   "strapi::security",
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      headers: "*",
-      origin: process.env.CORS_ORIGIN.split(",") || ["http://localhost:1337"],
-    },
-  },
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
   "strapi::query",
