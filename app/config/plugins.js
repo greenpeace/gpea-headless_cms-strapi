1a61dce29009ecea94c519d05c7b1504408e0b16
Chris Chan <chris.chan@greenpeace.org>
2022-09-29 23:12:04 +0800

    Add Content versioning plugin

--- app/config/plugins.js
+++ app/config/plugins.js
@@ -1,79 +1,80 @@
 // ./config/plugins.js
-module.exports = ({ env }) => {
-  return {
-    ckeditor: {
-      enabled: true,
-      config: {
-        plugin: {
-          styles: `
+module.exports = ({ env }) => ({
+  "content-versioning": {
+    enabled: true,
+  },
+  ckeditor: {
+    enabled: true,
+    config: {
+      plugin: {
+        styles: `
         .ck-dropdown__panel {
           max-width: 440px !important;
         }`,
+      },
+      editor: {
+        // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html
+        toolbar: {
+          items: [
+            "undo",
+            "redo",
+            "|",
+            "paragraph",
+            "heading1",
+            "heading2",
+            "heading3",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "fontColor",
+            "fontBackgroundColor",
+            "underline",
+            "blockQuote",
+            "bulletedList",
+            "numberedList",
+            "alignment",
+            "horizontalLine",
+            "|",
+            "StrapiMediaLib",
+            "insertImage",
+            "insertTable",
+            "mediaEmbed",
+            "htmlEmbed",
+            "sourceEditing",
+            "code",
+            "codeBlock",
+            "subscript",
+            "superscript",
+            "fullScreen",
+          ],
         },
-        editor: {
-          // https://ckeditor.com/docs/ckeditor5/latest/features/toolbar/toolbar.html
-          toolbar: {
-            items: [
-              "undo",
-              "redo",
-              "|",
-              "paragraph",
-              "heading1",
-              "heading2",
-              "heading3",
-              "|",
-              "bold",
-              "italic",
-              "link",
-              "fontColor",
-              "fontBackgroundColor",
-              "underline",
-              "blockQuote",
-              "bulletedList",
-              "numberedList",
-              "alignment",
-              "horizontalLine",
-              "|",
-              "StrapiMediaLib",
-              "insertImage",
-              "insertTable",
-              "mediaEmbed",
-              "htmlEmbed",
-              "sourceEditing",
-              "code",
-              "codeBlock",
-              "subscript",
-              "superscript",
-              "fullScreen",
-            ],
-          },
-          htmlSupport: {
-            allow: [
-              {
-                name: "img",
-                attributes: {
-                  sizes: true,
-                  loading: true,
-                },
+        htmlSupport: {
+          allow: [
+            {
+              name: "img",
+              attributes: {
+                sizes: true,
+                loading: true,
               },
-            ],
-          },
+            },
+          ],
         },
       },
     },
-    upload: {
-      config: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
       },
     },
-  };
-};
+  },
+});
