5f003616b7cb7a19d4f1374115b48c324e24bbb4
Chris Chan <chris.chan@greenpeace.org>
2022-09-29 11:38:29 +0800

    Update CSP list

--- app/config/middlewares.js
+++ app/config/middlewares.js
@@ -1,6 +1,41 @@
-module.exports = [
+module.exports = ({ env }) => [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "'unsafe-inline'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace
+            "greenpeace.org",
+            "api.greenpeace.org.hk",
+            "change.greenpeace.org.tw",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "res.cloudinary.com",
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace
+            "greenpeace.org",
+            "api.greenpeace.org.hk",
+            "change.greenpeace.org.tw",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
