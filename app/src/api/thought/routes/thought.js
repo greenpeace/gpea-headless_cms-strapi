ddcb24fb80a2efa9241505e833d92bda125841b5
KFC <chris.chan@greenpeace.org>
2022-12-08 10:56:43 +0800

    Delete unused Thought collection

--- app/src/api/thought/routes/thought.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * thought router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::thought.thought');
--- app/src/api/thought/services/thought.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * thought service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::thought.thought');
